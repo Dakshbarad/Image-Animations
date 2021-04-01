f5f1c2026437bd8ee83577442bc460b06b477467
Dakshbarad <daksh16121999@gmail.com>
2021-04-01 06:02:04 +0530

    First commit: Contains two animations

---
+++ ImageSlidersUsingSliderLibrary/js/main.js
@@ -0,0 +1,12 @@
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'cube',
+    grabCursor: true,
+    loop: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.54,
+    }
+  });
